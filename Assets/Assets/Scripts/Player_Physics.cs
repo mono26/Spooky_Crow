d71a00d32472904d6585add0ba33f27c23c1b171
mono26 <nzubiria1@gmail.com>
2017-01-18 10:03:32 -0500

    Escena de la mecanica

---
+++ Assets/Assets/Scripts/Player_Physics.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Physics : MonoBehaviour
+{
+    public Vector3 mousePosition;
+
+    private Rigidbody2D my_RigidBody;
+
+    void Awake()
+    {
+        my_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CastRay();      //Se necesita saber la posicion de el click
+        }
+    }
+
+    void CastRay()
+    {
+        RaycastHit2D hit;
+        Ray2D ray = new Ray2D(Input.mousePosition, Vector3.forward);
+    }
+
+    void MovePlayer(float forward, float horizontal)       //Metodo usado para mover al jugador, giroscopio, test teclas
+    {
+        var forces = Vector2.zero;
+        forces += Vector2.right * forward;
+        forces += Vector2.up * horizontal;
+        my_RigidBody.AddForce(forces * Time.deltaTime);
+    }
+}
