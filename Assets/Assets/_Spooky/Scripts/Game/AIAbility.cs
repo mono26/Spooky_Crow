3d0efc49d465a1daa3df97f047a730cb1a094cc7
Mono2692 <nzubiria1@gmail.com>
2017-08-21 15:46:07 -0500

    Minor Fix

--- Assets/Assets/_Spooky/Scripts/Game/AIAbility.cs
+++ Assets/Assets/_Spooky/Scripts/Game/AIAbility.cs
@@ -3,9 +3,7 @@
 [System.Serializable]
 public abstract class AIAbility : ScriptableObject
 {
-    [HideInInspector]
     public string abilityName;      //El nombre de la habilidad solo porque si, por bonito
-    [HideInInspector]
     public float abilityCooldown;       //El cooldown que tiene cada una de las habilidades
 
     public abstract void Ability(AIController controller);
---
+++ Library/Collab/Download/Assets/Assets/_Spooky/Scripts/Game/AIAbility.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public abstract class AIAbility : ScriptableObject
+{
+    public string abilityName;      //El nombre de la habilidad solo porque si, por bonito
+    public float abilityCooldown;       //El cooldown que tiene cada una de las habilidades
+
+    public abstract void Ability(AIController controller);
+}
